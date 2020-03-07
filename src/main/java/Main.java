d3923e4cc6bfa879952a99f50394d2e60d740600
koduki <koduki@users.noreply.github.com>
2020-03-07 00:44:30 -0800

    Add statement and resultset.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,7 @@
 
-import cn.orz.pascal.jdbc.MyConnection;
-import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author koduki
@@ -19,12 +12,13 @@ public class Main {
         String url = "jdbc:myjdbc://localhost:80/testdb";
 
         Class.forName("cn.orz.pascal.jdbc.MyDriver");
-        try (Connection con = DriverManager.getConnection(url)) {
-            if (con instanceof MyConnection) {
-                System.out.println("Load success!");
+        try (var con = DriverManager.getConnection(url); var st = con.createStatement()) {
+            st.execute("INSERT DUMMY SQL");
+            try (var rs = st.executeQuery("SELECT DUMMY SQL")) {
+                while (rs.next()) {
+                    System.out.println("rs[1]=" + rs.getString(1));
+                }
             }
-
         }
-        System.out.println("End");
     }
 }
