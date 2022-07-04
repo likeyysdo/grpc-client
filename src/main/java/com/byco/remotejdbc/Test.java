5ab19d4a01d695970ea09a895cc2282f48e16ca1
byco <likeyysdo@gmail.com>
2022-07-04 15:32:17 +0800

    rpc init

---
+++ src/main/java/com/byco/remotejdbc/Test.java
@@ -0,0 +1,16 @@
+package com.byco.remotejdbc;
+
+import java.util.ArrayList;
+import java.util.stream.IntStream;
+
+/**
+ * @Classname Test
+ * @Description TODO
+ * @Date 2022/6/28 16:58
+ * @Created by byco
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
